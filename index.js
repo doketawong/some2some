ba39f79b1e66f43a4a676739d0083e1775931c98
DESKTOP-4RADET5\admin <jason.wong@sixfacet.com>
2023-07-12 14:37:57 +0800

    add createDate

--- index.js
+++ index.js
@@ -8,55 +8,85 @@ const csvWriter = createCsvWriter({
     {id: 'content', title: 'Content'},
     {id: 'title', title: 'Title'},
     {id: 'price', title: 'Price'},
-    {id: 'picture', title: 'Picture'}
+    {id: 'picture', title: 'Picture'},
+    {id: 'size', title: 'Size'},
+    {id: 'createDate', title: 'Create Date'}
   ]
 })
 
 const url = 'https://graph.facebook.com/v17.0/';
-const access_token = 'EAADg4X8ZAH5QBAGfYMHnd4qJDRzKsxatJ7ZBrzQtJjbFMlpPZCfJmL7hbZAhW42KoSg2aZBQ1cdGfmIR02wNnaCPL4UqwLZCLWsf7IaQ7ZCcPDRDGfUmDpR0BFW1VGTt9htZCzWrIZBc25xKetKJuncG1HrkYpZBNesmpsHRDdeFCAZBDeuuOZBQTgpmKFuFwigNghBBzjFbwanbnx8IOxnUhGEb';
+const access_token = 'EAADg4X8ZAH5QBAMdeLJsOOTtSyOBg4mpaxZCJAK2JhVHdx3keIPo0R8am5XGwKnq9jYaXEWWDestJDpnckxw8tl2CsP8D9MJToJQISrMKO0vovUSj5opV2Itf95IYbA0swP4UgZCFVrA17RZClEzkyuNg1hZC1JVTXq3IYn6o3qkZBCLZBEqsKK87fMLZAGKZCogORsJxWMDk8CPGyfcqu5W1';
 const picture = '/attachments?access_token=';
 var dataArr = [];
 csv.parseFile("instagram.csv", {headers: true})
 .on('data', row => {
-
-    var tempPicture;
-    var tempPrice;
-    axios.get(url + row.No + picture +access_token)
-    .then (function (tempResponse) {
-      // handle success
-      var pictureUrl = tempResponse.data.data[0];
-      tempPicture = pictureUrl.media.image.src;
-
-      const priceIndex = row.Content.indexOf('$');
-
-      const title = getTitle(row.Content, priceIndex);
-
-      if(priceIndex > 0){
-          var price = row.Content.substring(priceIndex, priceIndex+4);
-          if(!isNumber(price.substring(price.length-1))){
-              price = price.substring(0, price.length-1);
-          }
-          tempPrice = price;
-      }
-      dataArr.push({id:row.No, content:row.Content, price:tempPrice, picture:tempPicture});
-  
-      
-    })
+  processContent(row); 
 
 }
-).on('end', end => 
-csvWriter.writeRecords(dataArr));
+)
+
+
+function processContent(row){
+  axios.get(url + row.No + picture +access_token)
+  .then (function (tempResponse) {
+    // handle success\
+    var pictureUrl = tempResponse.data.data[0]; 
+    const picture = pictureUrl.media.image.src;
+    const content = row.Content.replace(/(\r\n|\n|\r)/gm, "").trim();
+    const title = getTitle(content);
+    const price = getPrice(content);
+    const size = getSize(content);
+    const createDate = row.CreateDate;
+
+
+    dataArr.push({id:row.No, content:content, title: title,price:price, picture:picture, size: size, createDate: createDate});
+    console.log(dataArr.length);
+    if(dataArr.length>= 22){
+      csvWriter.writeRecords(dataArr);
+      console.log('end'); 
+    }
+})
+}
 
 
 function isNumber(char) {
-    return /^\d$/.test(char);
-  }
+    return /^\d$/.test(char); 
+}
 
-  function getTitle(content, priceIndex) {
-    const sortedString = content.substring(0,priceIndex)
-    const hyphenIndex = content.indexOf('-');
-    if(hyphenIndex > 0){
+function getTitle(content) {
+  const titleOpen = content.indexOf('【');
+  const titleClose = content.indexOf('】');
+  if(titleOpen < 0 || titleClose < 0){
+    return '';
+  }
+  const sortedString = content.substring(titleOpen+1, titleClose);
+  return sortedString;
+}
+
+function getSize(content) {
+  const sizeOpen = content.indexOf('/');
+  const sizeDesc = content.substring(sizeOpen+2, content.length);
+  const sizeClose = sizeDesc.indexOf('/');
+  if(sizeOpen < 0 || sizeClose < 0){
+    return '';
+  }
+  const sortedString = sizeDesc.substring(0, sizeClose);
+  return sortedString;
+}
+
+function getPrice(content) {
+  const priceIndex = content.indexOf('$');
+  
+  if(priceIndex > 0){
+    var price = content.substring(priceIndex + 1, priceIndex+4);
+    if(!isNumber(price.substring(price.length-1))){ 
+        price = price.substring(0, price.length-1);
     }
-
-    return /^\d$/.test(char);
+    tempPrice = price;
+  } else {
+    return '';
   }
+
+  return tempPrice;
+}
+
